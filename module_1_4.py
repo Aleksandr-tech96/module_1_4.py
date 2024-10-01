53f1a2be0c3d108d6eb668dd31b5c871de7fd442
Aleksandr-tech96 <alexkapustin96@gmail.com>
2024-10-01 23:06:23 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+input_my_string = "Укажите Ваше имя:"
+character_count = len(input_my_string)
+print("Количество символов:", character_count)
+print(input_my_string.upper())
+print(input_my_string.lower())
+print(input_my_string.replace(' ', ''))
+print(input_my_string[0])
+print(input_my_string[-1])
